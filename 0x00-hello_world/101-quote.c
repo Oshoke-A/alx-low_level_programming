00057268622cd18947e4e12a7b93509c0b194691
Oshoke-A <oakiotu@gmail.com>
2022-09-09 15:25:54 +0000

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+int main(void)
+{
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59, 1, stderr);
+	return (1);
+}  

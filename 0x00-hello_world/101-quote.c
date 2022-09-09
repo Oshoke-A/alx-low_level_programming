999033e034cf1d7ee662cec47f6fe2687091f570
Oshoke-A <oakiotu@gmail.com>
2022-09-09 15:47:57 +0000

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,13 @@
-#include <stdio.h>
 #include <unistd.h>
-#include <string.h>
-#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59, 1, stderr);
-	return (1);
-}  
+		char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+			write(2, str1, 59);
+				return (1);
+}

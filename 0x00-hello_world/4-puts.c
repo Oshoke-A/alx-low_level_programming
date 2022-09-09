15fd3ab9972063abc061339b9f09b7d8055c09ae
Oshoke-A <oakiotu@gmail.com>
2022-09-09 13:10:44 +0000

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
-#!/bin/bash
+#include <stdio.h>
 int main(void)
 {
-	int puts("\"Programming is like building a multilingual puzzle")
-		return (0)
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
 }

378bc0a16d1095a21182da9ea74fc1575ed097da
Oshoke-A <oakiotu@gmail.com>
2022-09-16 16:48:10 +0000

    l

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
+ *print_alphabet - prints the alphabet
  *
- *
- *
+ *Return: 0 Always (Success)
  */
 void print_alphabet(void)
 {
 	int i;
+
 	for (i=97; i < 123; i++)
 	{
 		_putchar(i);
 	}
-_putchar("\n");
+_putchar('\n');
 }

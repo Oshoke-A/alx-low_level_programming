e2e83e4b3b3ab2f98bef0e0a089b4f01c05a11be
Oshoke-A <oakiotu@gmail.com>
2022-09-16 20:59:56 +0000

    commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,5 +12,14 @@ int print_last_digit(int n)
 	int lastDigit;
 
 	lastDigit = n % 10;
-	return (lastDigit);
+	if (lastDigit < 0)
+	{
+		_putchar(-lastDigit + 48);
+		return (-lastDigit);
+	}
+	else
+	{
+		_putchar(lastDigit + 48);
+		return (lastDigit);
+	}
 }

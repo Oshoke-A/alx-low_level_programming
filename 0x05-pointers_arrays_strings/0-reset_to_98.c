ddf64a807f63d71d693f1f4e3a3bd73b6c9b2c6f
Oshoke-A <oakiotu@gmail.com>
2022-09-19 15:22:07 +0000

    Function to update with a pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value
+ * pointer n points to
+ *
+ * @n: the pointer parameter
+ *
+ * Return: no return value
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

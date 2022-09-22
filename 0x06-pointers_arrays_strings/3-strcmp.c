29d876e05f79fbc09637240c40dfe7884b2c820a
Oshoke-A <oakiotu@gmail.com>
2022-09-22 21:03:58 +0000

    Tested

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,6 +11,14 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	strcmp(s1, s2);
-	return (0);
+	int i = 0, op = 0;
+
+	while (op == 0)
+	{
+		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
+			break;
+		op = *(s1 + i) - *(s2 + i);
+		i++;
+	}
+	return (op);
 }

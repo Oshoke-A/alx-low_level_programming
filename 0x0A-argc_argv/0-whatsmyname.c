3638f580c1c2544ae58748ba583b9feda363acbb
Oshoke-A <oakiotu@gmail.com>
2022-09-30 14:57:26 +0000

    local

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of program
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: void
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,3 @@
+This Readme was created learning argc and argv
+argc and argv are arguments passed to main function 
+to enable you pass arguments from the command line

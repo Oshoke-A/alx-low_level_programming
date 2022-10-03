f051e4c5853f7238dd8eeb08c153c7e59ce0bf49
Oshoke-A <oakiotu@gmail.com>
2022-10-03 16:22:09 +0000

    common

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strdup - returns a copy of
+ * string given as parameter
+ *
+ * @str: string
+ *
+ * Return: NULL on failure
+ * copy of string on success
+ */
+
+char *_strdup(char *str)
+{
+	int i;
+	char *tempstr;
+
+	if (str == NULL)
+		return (NULL);
+
+	tempstr = malloc(sizeof(char) * strlen(str));
+
+	if (tempstr == NULL)
+		return (NULL);
+	
+	for (i = 0; str[i] != '\0'; i++)
+		tempstr[i] = str[i];
+
+	return (tempstr);
+	free(tempstr);
+}
